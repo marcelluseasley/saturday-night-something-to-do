5d5c511b7157a467e45ecad36c3d7156bb0dbc39
Marcellus Easley <marcellus.easley@gmail.com>
2024-02-18 21:36:23 -0500

    add implementation to get user data from user service

--- service/service.go
+++ service/service.go
@@ -23,3 +23,37 @@ func (u *UserService) CreateUser(user models.User) error {
 	}
 	return nil
 }
+
+func (u *UserService) GetUser(id int) (models.User, error) {
+	user, err := u.s.GetUser(id)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func (u *UserService) UpdateUser(user models.User) error {
+	err := u.s.UpdateUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserService) DeleteUser(id int) error {
+	err := u.s.DeleteUser(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserService) ListUsers() ([]models.User, error) {
+	users, err := u.s.ListUsers()
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+
